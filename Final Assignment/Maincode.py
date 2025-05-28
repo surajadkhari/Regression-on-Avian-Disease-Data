93e312f93e06aacdd3e0194fa3ecd47eac1aae43
Rupesh Pun <158485942+Ruphello@users.noreply.github.com>
2025-05-28 11:58:43 +1000

    Maincode.py

--- Final Assignment/Maincode.py
+++ Final Assignment/Maincode.py
@@ -1,1 +1,20 @@
-
+from google.colab import drive
+drive.mount('/content/drive')
+import pandas as pd
+df= pd.read_csv("/content/drive/MyDrive/Coding/final_avian_dataset.csv")
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import classification_report, confusion_matrix, ConfusionMatrixDisplay, accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, confusion_matrix, ConfusionMatrixDisplay, accuracy_score
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.cluster import KMeans
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder
